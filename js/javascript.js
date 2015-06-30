1074e5123e57139b0626c3ff68220d456a496e07
Carolin Klein <schueler@Carolin.local>
2015-07-01 01:08:58 +0200

    changes01.juli

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$(".btn_icon").click(function() {
+	/*$(".btn_icon").click(function() {
 		console.log("click");
 	});
 
@@ -12,7 +12,22 @@ $(document).ready(function(){
 
 	$(".btn").hover(function() {
 		$(".btn-text").toggleClass("btnActive");
-	});
+	});*/
 
+$( "button" ).click(function() {
+	$( ".dropdown-text" ).slideToggle( "slow" );
+});
 
 });
+
+function initialize()
+{
+	var mapOpt = {
+		center:new google.maps.LatLng(51.508742,-0.120850),
+		zoom:5,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapOpt);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
